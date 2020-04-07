ee9a571b82c0bdd7fb2db98561a801397984d8a5
marthon-szabo <makotoimx@gmail.com>
2020-04-07 16:51:13 +0200

    Declare FilePartReader.setup function

---
+++ src/hu/marthon_szabo/file_part_reader/FilePartReader.java
@@ -0,0 +1,7 @@
+package hu.marthon_szabo.file_part_reader;
+
+public class FilePartReader {
+    public  void setup(String filePath, Integer fromLine, Integer toLine) {
+
+    }
+}
---
+++ src/hu/marthon_szabo/file_part_reader/Main.java
@@ -0,0 +1,8 @@
+package hu.marthon_szabo.file_part_reader;
+
+public class Main {
+    public static void main(String[] args) {
+        FilePartReader reader = new FilePartReader();
+        reader.setup();
+    }
+}
