6e6e07ef5dcb595837fc97e90d42b6e12ee67762
marthon-szabo <makotoimx@gmail.com>
2020-04-10 17:11:32 +0200

    Add FileWorldAnalyzerTest class

--- src/hu/marthon_szabo/file_part_reader/FileWorldAnalyzer.java
+++ src/hu/marthon_szabo/file_part_reader/FileWorldAnalyzer.java
@@ -1,4 +1,15 @@
 package hu.marthon_szabo.file_part_reader;
 
+import java.util.List;
+
 public class FileWorldAnalyzer {
+    private FilePartReader fpr;
+
+    public FileWorldAnalyzer(FilePartReader fpr) {
+        this.fpr = fpr;
+    }
+
+    public List getWordsContainingSubstrings() {
+
+    }
 }
---
+++ tests/hu/marthon_szabo/file_part_reader/FileWorldAnalyzerTest.java
@@ -0,0 +1,7 @@
+package hu.marthon_szabo.file_part_reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileWorldAnalyzerTest {
+
+}
